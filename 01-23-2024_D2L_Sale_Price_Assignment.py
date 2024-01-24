20869177ad9c0cc785c7771c2fc43d9f6c8e6d96
Zamezetor <149831319+Zamezetor@users.noreply.github.com>
2024-01-23 18:54:03 -0700

    Upload 01-23-2024_D2L_Sale_Price_Assignment.py

---
+++ 01-23-2024_D2L_Sale_Price_Assignment.py
@@ -0,0 +1,11 @@
+# Logan White 01/23/2024
+# 01-23-2024_D2L_Sale_Price_Assignment.py
+# Sale Price Assignment for D2L
+# CC BY-NC-SA 4.0
+# Variables
+# Functions
+# Code
+original_price = float(input("Enter the item's original price: "))
+discount = original_price * 0.2
+sale_price = original_price - discount
+print('The sale price is', sale_price)
