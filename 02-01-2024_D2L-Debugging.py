e68523a44b680861d157756821d0af2263a6ee56
Zamezetor <149831319+Zamezetor@users.noreply.github.com>
2024-02-01 18:37:02 -0700

    Upload PEP-8_Template.py and 02-01-2024_D2L-Debugging.py

---
+++ 02-01-2024_D2L-Debugging.py
@@ -0,0 +1,21 @@
+# Logan White 02/01/2024
+# 02-01-2024_D2L-Debugging.py
+# Debugging Activity on D2L 
+# CC BY-NC-SA 4.0
+# Imports 
+
+
+# Variables 
+
+
+# Functions
+def check_equality(num1,num2):
+    if num1 == num2:
+        print("The Values are Equal")
+    else:
+        print("The Values are NOT Equal")
+
+
+# Code
+check_equality(input("What is the First Number: "), input("What is the Second Number: "))
+
---
+++ PEP-8_Template.py
@@ -0,0 +1,8 @@
+# Logan White --/--/----
+# XX-XX-XXXX_{Name}.py
+# Purpose 
+# CC BY-NC-SA 4.0
+# Imports 
+# Variables 
+# Functions
+# Code
