8c8030d6269371f8043b5525286f01bf4d9e92a4
Zamezetor <149831319+Zamezetor@users.noreply.github.com>
2024-02-15 18:50:41 -0700

    Upload 02-15-2024_D2L_module6-EC.py

---
+++ 02-15-2024_D2L_module6-EC.py
@@ -0,0 +1,14 @@
+# Logan White 02/15/2024
+# 02-15-2024_D2L_module6-EC.py
+# D2L module 6 Extra Credit Lab 
+# CC BY-NC-SA 4.0
+# Imports 
+# Variables 
+sale_tax = 0.06
+burger_price = .99
+fries_price = .79
+soda_price = 1.09
+# Functions
+def main():
+    pass
+# Code
